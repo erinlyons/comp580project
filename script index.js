2a7cd8470d5c6f61a2ca59892f1faa5bd9098810
Erin Lyons <eglyons@live.unc.edu>
2020-04-30 02:05:36 -0400

    edits

--- script index.js
+++ script index.js
@@ -13,25 +13,23 @@ function say(s)
   
   // easily wait for sounds
   
-  
- 
-  
-     say("Welcome to the Jam Session");
-  say("This game will give you a chance to play your favorite instruments and learn the notes in the scale in the most fun and easy way. Have fun and make some music!");
+ say("Welcome to the Jam Session");
+ say("This game will give you a chance to play your favorite instruments and learn the notes in the scale in the most fun and easy way. Have fun and make some music!");
  say("Press ENTER to learn how to play. Press SPACE to start game.");
   
- 
+ function cancelAudio(){
+   speechSynthesis.cancel();
+ }
 
-  
-  
+  document.getElementsByName('button').addEventListener('click', cancelAudio);
 
   document.addEventListener('keydown', event => {
     if (event.keyCode == 13) {
         document.getElementById("Help").click(); 
-        speechSynthesis.cancel();
+        cancelAudio();
     } else if (event.keyCode == 32) {
         document.getElementById("Start").click();
-        speechSynthesis.cancel(); 
+        cancelAudio(); 
     }
 });
   
