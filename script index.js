b0ed94aa88c26727a1d518c6a6d69c0d8c962b6d
Erin Lyons <eglyons@live.unc.edu>
2020-05-02 11:20:50 -0400

    homepage

--- script index.js
+++ script index.js
@@ -18,7 +18,7 @@ function say(s)
   
 say("Welcome to the Jam Session");
  say("This game will give you a chance to play your favorite instruments and learn the notes in the scale in the most fun and easy way. Have fun and make some music!");
- say("Press ENTER to learn how to play. Press SPACE to start game.");
+ say("Press ENTER to learn how to play. Press SPACE to start game. Press any other key to hear these instructions read aloud again.");
   
  
 
